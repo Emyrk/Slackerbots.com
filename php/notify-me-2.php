f2fd47e101ffc3558db87aec06d63a76ad7b203f
Steven Masley <steven@slackerbots.com>
2015-11-02 16:58:14 -0500

    Fixed notify form
    added form options to notify form, and changed loading name

--- php/notify-me-2.php
+++ php/notify-me-2.php
@@ -6,12 +6,12 @@ header('content-type: application/json');
 	$o->status = 'success';
 	echo json_encode($o);
 
-	$email_to = "jb08090@gmail.com"; // Replace by your email address
+	$email_to = "hi@slackerbots.com"; // Replace by your email address
 	$email = $_POST["email"];
 	$text = "Congratulations ! A new person wants to be alerted when your site will be online: $email";
 
-	$headers = "MIME-Version: 1.0" . "\r\n"; 
-	$headers .= "Content-type:text/html; charset=utf-8" . "\r\n"; 
+	$headers = "MIME-Version: 1.0" . "\r\n";
+	$headers .= "Content-type:text/html; charset=utf-8" . "\r\n";
 	$headers .= "From:<$email>\n";
 
 	mail($email_to, "Message", $text, $headers);
--- php/notify-me.php
+++ php/notify-me.php
@@ -8,18 +8,20 @@
 $STORE_MODE = "mailchimp";
 
 // MailChimp API Key findable in your Mailchimp's dashboard
-$API_KEY =  "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX-XX0";
-			 
+$API_KEY =  "ea9b455ee753bb08aa14e9aa4a9e7421-us12";
+
 // MailChimp List ID  findable in your Mailchimp's dashboard
-$LIST_ID =  "XXXXXXXXXX";
-			 
+$LIST_ID =  "7293513aa9";//List for peeps who need bots
+$NONE_LIST_ID =  "f05af1b9b0";//List for neither
+$DEV_LIST_ID =  "95c2be42a2"; //List for devs
+
 // After $_SERVER["DOCUMENT_ROOT"]." , write the path to your .txt to save the emails of the subscribers
 $STORE_FILE = $_SERVER["DOCUMENT_ROOT"]."/subscription-list.txt";
 
 /* ************************************ */
 // Don't forget to check the path below
 /* ************************************ */
- 
+
 require('MailChimp.php');
 
 /* ***************************************************** */
@@ -29,17 +31,17 @@ require('MailChimp.php');
 if($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST["email"])) {
 
 	$email = $_POST["email"];
-	
+	$dev = $_POST["dev"];
 	header('HTTP/1.1 200 OK');
 	header('Status: 200 OK');
 	header('Content-type: application/json');
 
 	// Checking if the email writing is good
 	if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
-		
+
 		// The part for the storage in a .txt
 		if ($STORE_MODE == "file") {
-			
+
 			// SUCCESS SENDING
 			if(@file_put_contents($STORE_FILE, strtolower($email)."\r\n", FILE_APPEND)) {
 				echo json_encode(array(
@@ -52,23 +54,41 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST["email"])) {
 						"type" => "FileAccessError"
 					));
 			}
-		
+
 		// The part for the storage in Mailchimp
 		} elseif ($STORE_MODE == "mailchimp") {
-			
+
 			$MailChimp = new \Drewm\MailChimp($API_KEY);
-			
-			$result = $MailChimp->call('lists/subscribe', array(
-		                'id'                => $LIST_ID,
-		                'email'             => array('email'=>$email),
-		                'double_optin'      => false,
-		                'update_existing'   => true,
-		                'replace_interests' => false,
-		                'send_welcome'      => true,
-		            ));     
-	
+			if ($dev == "true"){
+				$result = $MailChimp->call('lists/subscribe', array(
+											'id'                => $DEV_LIST_ID,
+											'email'             => array('email'=>$email),
+											'double_optin'      => false,
+											'update_existing'   => true,
+											'replace_interests' => false,
+											'send_welcome'      => true,
+									));
+			} elseif($dev == "false"){
+				$result = $MailChimp->call('lists/subscribe', array(
+			                'id'                => $LIST_ID,
+			                'email'             => array('email'=>$email),
+			                'double_optin'      => false,
+			                'update_existing'   => true,
+			                'replace_interests' => false,
+			                'send_welcome'      => true,
+			            ));
+			} else{
+				$result = $MailChimp->call('lists/subscribe', array(
+			                'id'                => $NONE_LIST_ID,
+			                'email'             => array('email'=>$email),
+			                'double_optin'      => false,
+			                'update_existing'   => true,
+			                'replace_interests' => false,
+			                'send_welcome'      => true,
+			            ));
+			}
 			// SUCCESS SENDING
-			if($result["email"] == $email) {     	
+			if($result["email"] == $email) {
 				echo json_encode(array(
 						"status" => "success"
 					));
@@ -85,7 +105,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST["email"])) {
 					"status" => "error",
 				));
 		}
-	// ERROR DURING THE VALIDATION 
+	// ERROR DURING THE VALIDATION
 	} else {
 		echo json_encode(array(
 				"status" => "error",
